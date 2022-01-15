a66246d6b0051151ff14d4f00451a1e26b1d93c7
Skylortrexler <42103122+Skylortrexler@users.noreply.github.com>
2022-01-15 10:26:39 -0800

    Initial commit

---
+++ src/main/java/website/skylorbeck/magehand/Magehand.java
@@ -0,0 +1,10 @@
+package website.skylorbeck.magehand;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Magehand implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/website/skylorbeck/magehand/client/MagehandClient.java
@@ -0,0 +1,13 @@
+package website.skylorbeck.magehand.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class MagehandClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
