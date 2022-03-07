0d534b22d4ec406d85cd0767fa3774b29707fbb5
Skylortrexler <42103122+Skylortrexler@users.noreply.github.com>
2022-03-07 10:45:33 -0800

    EntityRendererRegistryImpl
    Signed-off-by: Skylortrexler <42103122+Skylortrexler@users.noreply.github.com>

--- src/main/java/website/skylorbeck/magehand/client/MagehandClient.java
+++ src/main/java/website/skylorbeck/magehand/client/MagehandClient.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
+import net.fabricmc.fabric.impl.client.rendering.EntityRendererRegistryImpl;
 import website.skylorbeck.magehand.Declarar;
 import website.skylorbeck.magehand.entity.MageHandRenderer;
 
@@ -11,11 +12,11 @@ import website.skylorbeck.magehand.entity.MageHandRenderer;
 public class MagehandClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        EntityRendererRegistry.INSTANCE.register(Declarar.MAGE_HAND_HOSTILE_ENTITY_TYPE, MageHandRenderer::new);
-        EntityRendererRegistry.INSTANCE.register(Declarar.MAGE_HAND_COPPER_ENTITY_TYPE, MageHandRenderer::new);
-        EntityRendererRegistry.INSTANCE.register(Declarar.MAGE_HAND_IRON_ENTITY_TYPE, MageHandRenderer::new);
-        EntityRendererRegistry.INSTANCE.register(Declarar.MAGE_HAND_GOLD_ENTITY_TYPE, MageHandRenderer::new);
-        EntityRendererRegistry.INSTANCE.register(Declarar.MAGE_HAND_DIAMOND_ENTITY_TYPE, MageHandRenderer::new);
-        EntityRendererRegistry.INSTANCE.register(Declarar.MAGE_HAND_AMETHYST_ENTITY_TYPE, MageHandRenderer::new);
+        EntityRendererRegistryImpl.register(Declarar.MAGE_HAND_HOSTILE_ENTITY_TYPE, MageHandRenderer::new);
+        EntityRendererRegistryImpl.register(Declarar.MAGE_HAND_COPPER_ENTITY_TYPE, MageHandRenderer::new);
+        EntityRendererRegistryImpl.register(Declarar.MAGE_HAND_IRON_ENTITY_TYPE, MageHandRenderer::new);
+        EntityRendererRegistryImpl.register(Declarar.MAGE_HAND_GOLD_ENTITY_TYPE, MageHandRenderer::new);
+        EntityRendererRegistryImpl.register(Declarar.MAGE_HAND_DIAMOND_ENTITY_TYPE, MageHandRenderer::new);
+        EntityRendererRegistryImpl.register(Declarar.MAGE_HAND_AMETHYST_ENTITY_TYPE, MageHandRenderer::new);
     }
 }
